5e9cb87f7ab4882c4148ae871352bddc8330dcbd
enricobarbierato <enrico.barbierato@polimi.it>
2017-03-06 14:15:31 +0100

    Add files via upload

--- simlib/heap.c
+++ simlib/heap.c
@@ -1,15 +1,21 @@
 /*
- * heap.c
+## Copyright 2017 Enrico Barbierato <enrico.barbierato@polimi.it>
+## Copyright 2017 Marco Gribaudo <marco.gribaudo@polimi.it>
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+*/
 
- *
- *  Created on: 13 mar 2016
- *      Author: Enrico
- */
 
-
-/*
- *
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
